1970c9dde1673c52f2ad7958f5e83aa43bc6d3c1
amankkg <amankubanychbek@gmail.com>
2021-02-19 21:39:50 +0600

    add crud api

---
+++ RestCrud/Controllers/CategoriesController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RestCrud.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace RestCrud.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private ICategoryRepository _db;
+
+        public CategoriesController(ICategoryRepository db)
+        {
+            _db = db;
+        }
+        // GET: api/<CategoriesController>
+        [HttpGet]
+        public IEnumerable<Category> Get()
+        {
+            return _db.GetAll();
+        }
+
+        // GET api/<CategoriesController>/5
+        [HttpGet("{id}")]
+        public Category Get(int id)
+        {
+            return _db.Find(id);
+        }
+
+        // POST api/<CategoriesController>
+        [HttpPost]
+        public (string, Category) Post([FromBody] Category category)
+        {
+            _db.Add(category);
+
+            return ($"/api/categories/{category.Id}", category);
+        }
+
+        // PUT api/<CategoriesController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Category category)
+        {
+            category.Id = id;
+
+            _db.Edit(category);
+        }
+
+        // DELETE api/<CategoriesController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            _db.Remove(id);
+        }
+    }
+}
---
+++ RestCrud/Model/CategoryRepository.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestCrud.Model
+{
+    public interface ICategoryRepository
+    {
+        IEnumerable<Category> GetAll();
+        Category Find(int id);
+        void Add(Category c);
+        void Edit(Category c);
+        void Remove(int id);
+    }
+
+    public class CategoryListRepository : ICategoryRepository
+    {
+        private int index = 0;
+        List<Category> categories = new List<Category>();
+
+        public IEnumerable<Category> GetAll() => categories;
+
+        public Category Find(int id) => categories.FirstOrDefault(c => c.Id == id);
+
+        public void Add(Category c)
+        {
+            c.Id = ++index;
+            categories.Add(c);
+        }
+
+        public void Edit(Category c)
+        {
+            categories = categories.Where(cc => cc.Id == c.Id).ToList();
+            categories.Add(c);
+        }
+
+        public void Remove(int id)
+        {
+            categories = categories.Where(c => c.Id != id).ToList();
+        }
+    }
+}
